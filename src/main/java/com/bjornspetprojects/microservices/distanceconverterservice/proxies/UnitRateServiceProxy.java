e6e04c65ca165d5e9304c7012220247edef719f5
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-28 18:41:45 +0100

    Implemented multiple servers with Ribbon

--- src/main/java/com/bjornspetprojects/microservices/distanceconverterservice/proxies/UnitRateServiceProxy.java
+++ src/main/java/com/bjornspetprojects/microservices/distanceconverterservice/proxies/UnitRateServiceProxy.java
@@ -1,11 +1,13 @@
 package com.bjornspetprojects.microservices.distanceconverterservice.proxies;
 
 import com.bjornspetprojects.microservices.distanceconverterservice.model.UnitRate;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "unit-rate-service", url="localhost:8000")
+@FeignClient(name = "unit-rate-service")
+@RibbonClient(name = "unit-rate-service")
 public interface UnitRateServiceProxy {
     @GetMapping("/unit-rate/from/{from}/to/{to}")
     UnitRate getUnitRate(@PathVariable String from, @PathVariable String to);
