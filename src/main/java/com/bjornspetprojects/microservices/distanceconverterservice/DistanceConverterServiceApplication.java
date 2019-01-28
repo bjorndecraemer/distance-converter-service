5f155d2924ef1f2c322a291e5f0a4e19c935d5fe
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-28 21:07:01 +0100

    Integrate with netflix Eureka naming service

--- src/main/java/com/bjornspetprojects/microservices/distanceconverterservice/DistanceConverterServiceApplication.java
+++ src/main/java/com/bjornspetprojects/microservices/distanceconverterservice/DistanceConverterServiceApplication.java
@@ -2,12 +2,13 @@ package com.bjornspetprojects.microservices.distanceconverterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.bjornspetprojects.microservices.distanceconverterservice")
+@EnableDiscoveryClient
 public class DistanceConverterServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DistanceConverterServiceApplication.class, args);
 	}
