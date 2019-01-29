cdf995409fe430fdc533dddc6b2e96ba24177b53
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-29 01:25:16 +0100

    Implement Netflix Zuul Service

--- src/main/java/com/bjornspetprojects/microservices/distanceconverterservice/proxies/UnitRateServiceProxy.java
+++ src/main/java/com/bjornspetprojects/microservices/distanceconverterservice/proxies/UnitRateServiceProxy.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "unit-rate-service")
+@FeignClient(name = "unit-conversion-netflix-zuul-service")
 @RibbonClient(name = "unit-rate-service")
 public interface UnitRateServiceProxy {
-    @GetMapping("/unit-rate/from/{from}/to/{to}")
+    @GetMapping("/unit-rate-service/unit-rate/from/{from}/to/{to}")
     UnitRate getUnitRate(@PathVariable String from, @PathVariable String to);
 }
